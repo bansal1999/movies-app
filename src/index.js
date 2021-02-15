f11191bbefa60e35734709d5e56edb9af92c3bd2
ShikharBansal <shikharbansal007@gmail.com>
2021-02-15 15:10:26 +0530

    subscribed to the store

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
 
-
 import './index.css';
 import App from './components/App';
 import movies from './reducers';
 
- 
 const store =  createStore( movies);
 console.log('store', store);
 
+// console.log('state', store.getState());
+
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies: [{name: 'Superman'}]
+// });
+
+// console.log('BEFORE STATE', store.getState());
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App store={store} />,
+    document.getElementById('root')
 );
  
